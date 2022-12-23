5ad1db89aa019d64adba71c74129ca77577b28db
Tungusova <99951246+lzvtmrnv@users.noreply.github.com>
2022-12-23 10:28:55 +0700

    Create task2.java

---
+++ lab_4/task2.java
@@ -0,0 +1,19 @@
+package Lab_4;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        double sum = 1;
+        for (int i = 2; i <= 10000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(Math.pow(i, -1) + " minus ");
+                sum -= Math.pow(i, -1);
+            }
+            else {            
+            System.out.println(Math.pow(i, -1) + " plus ");
+            sum += Math.pow(i, -1);
+            }
+        }
+        System.out.println(sum);
+    }
+    
+}
