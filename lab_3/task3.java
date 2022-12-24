963f0140ab484667194c1f94e1d9b6bcbfe4bc6c
Tungusova <99951246+lzvtmrnv@users.noreply.github.com>
2022-12-24 11:25:20 +0700

    Update task3.java

--- lab_3/task3.java
+++ lab_3/task3.java
@@ -15,8 +15,8 @@ public class Task_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("enter a, b: ");
-        int a = in.nextInt();
-        int b = in.nextInt();
+        double a = in.nextDouble();
+        double b = in.nextDouble();
         System.out.println("Summa: " + (a+b));
         System.out.println("Raznost: " + (a-b));
         System.out.println("Mult: " + (a*b));
