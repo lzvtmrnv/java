101b70e975f114bd0426e37de95948a76a505c5d
Tungusova <99951246+lzvtmrnv@users.noreply.github.com>
2022-12-28 14:38:50 +0700

    Update task4.java

--- lab_6/task4.java
+++ lab_6/task4.java
@@ -1,7 +1,7 @@
 //project
-package lab_6;
+package laba_6;
 import java.util.Scanner;
-public class Task_4 {
+public class Task4 {
     public static void main(String[] args) {
         double dollars, rub;
         final double course = 72.12;
