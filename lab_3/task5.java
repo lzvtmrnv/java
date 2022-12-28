6af29169b761ceb1fc7bf3c70e466b055f32e6a4
Tungusova <99951246+lzvtmrnv@users.noreply.github.com>
2022-12-28 14:35:00 +0700

    Update task5.java

--- lab_3/task5.java
+++ lab_3/task5.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
+* To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lab_3;
+package laba_3;
 
 import java.util.Scanner;
 
@@ -11,7 +10,7 @@ import java.util.Scanner;
  *
  * @author Student
  */
-public class Task_5 {
+public class Task5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("enter a,c: ");
