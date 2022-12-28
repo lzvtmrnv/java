af55d507a0736dd50fadfe9c6651d631cb16f85f
Tungusova <99951246+lzvtmrnv@users.noreply.github.com>
2022-12-28 14:33:20 +0700

    Update task1.java

--- lab_3/task1.java
+++ lab_3/task1.java
@@ -1,28 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Lab_3;
+package laba_3;
 import java.util.Scanner;
-/**
- *
- * @author Student
- */
-public class Task_1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("enter n: ");
-        int n = in.nextInt();
-        int rez = 0;
-        for (int i = 0; i < n; i++) {
-            rez = (int) Math.pow (2, n);
-        }
-        System.out.println(rez);
-    }
-    
+public class Task1
+{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		int res = 1;
+		System.out.print("Enter n: ");
+		int n = console.nextInt();
+		for (int i = 0; i < n; i++) {
+			res *= 2;
+		}
+		System.out.println("2^" + n + " = " + res);
+	}
 }
